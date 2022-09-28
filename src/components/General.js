1106f2ac3075cbc456a03d84c038dc59ed781a6c
Almeida-91 <aplalmeida91@outlook.com>
2022-09-28 21:37:58 +0100

    Add General component

---
+++ src/components/General.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class General extends Component {
+
+
+    render(){
+        return(
+            <div>
+                General
+                <form>
+                    <div>
+                        <label htmlFor="name">Name:</label>
+                        <input type={"text"} id="name"></input>
+                    </div>
+                    <div>
+                        <label htmlFor="email">E-mail:</label>
+                        <input type={"email"} id="email"></input>
+                    </div>
+                    <div>
+                        <label htmlFor="phoneNo">Phone Number:</label>
+                        <input type={"tel"} id="phoneNo"></input>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default General;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import General from './components/General';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <General />
+  </React.StrictMode>
+);
