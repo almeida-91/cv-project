d3d20973caefdef9a80c7cc23eddfa429f0913f7
Almeida-91 <aplalmeida91@outlook.com>
2022-09-29 21:23:41 +0100

    Clean folder

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import Education from './components/Education';
 import General from './components/General';
 import Practice from './components/Practice';
+// eslint-disable-next-line no-unused-vars
 import styles from './styles/styles.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
